f85bc6b0a239d90ee85afdfcc66b631238487be1
Zakaria Mekelleche <zmekell@gmail.com>
2020-10-06 22:35:40 +0100

    add mission and collection section

--- script/script.js
+++ script/script.js
@@ -3,8 +3,24 @@ $(document).ready(function () {
     $('.nav-button').toggleClass('change');
   })
 
-  $(window) scroll(function () {
+  $(window).scroll(function () {
     let position = $(this).scrollTop();
-    console.log(position);
+    if (position >= 200) {
+      $('.nav-menu').addClass('custom-navbar');
+    } else {
+      $('.nav-menu').removeClass('custom-navbar');
+    }
   })
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position >= 450) {
+      $('.camera').addClass('fromleft');
+      $('.paragraph').addClass('fromright');
+    } else {
+      $('.camera').removeClass('fromleft');
+      $('.paragraph').removeClass('fromright');
+    }
+  })
+
 });
