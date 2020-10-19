9bb213a757145bf7891120feeaf053605c56d487
Zakaria Mekelleche <zmekell@gmail.com>
2020-10-19 17:31:30 +0100

    add contact & pricing section in HTML and CSS and JS

--- script/script.js
+++ script/script.js
@@ -22,5 +22,35 @@ $(document).ready(function () {
       $('.paragraph').removeClass('fromright');
     }
   })
+  
+  $('.gallery-list').click(function () {
+    let value = $(this).attr('data-filter');
+    if (value === 'all'){
+      $('.filter').show(300);
+    } else {
+      $('.filter').not('.' + value).hide(300);
+      $('.filter').filter('.' + value).show(300);
+    }
+  })
+
+  $('.gallery-list').click(function () {
+    $(this).addClass('active-list').siblings().removeClass('active-list');
+  })
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    
+    if (position >= 3700) {
+      $('.card-1').addClass('movefromLeft');
+      $('.card-3').addClass('movefromRight');
+      $('.card-2').addClass('movefromBottom');
+    }else {
+      $('.card-1').removeClass('movefromLeft');
+      $('.card-3').removeClass('movefromRight');
+      $('.card-2').removeClass('movefromBottom');
+    }
+  });
 
 });
+
+
